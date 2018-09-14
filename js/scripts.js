895b77e8c583f4423a35e9b3341e700ae7b4f8b6
nignanthomas <nignanthomas@gmail.com>
2018-09-14 16:00:57 +0300

    rewrite the triangle tracker script and light changes to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,92 @@
 var $ = function(id) { return document.getElementById(id); }
 
+// var validLength = false;
+// var trackerResult = " ";
 
-var lengtha = $("lengtha");
-var lengthb = $("lengthb");
-var lengthc = $("lengthc");
-var trackerResult;
 
-// var triangleSides = [$("lengtha"), $("lengthb"), $("lengthc")];
 //THIS FUNCTION WILL RETURN THE RESULT OF THE TRACKER IN HTML
 var display = function (){
+
   var lengtha = $("lengtha");
   var lengthb = $("lengthb");
   var lengthc = $("lengthc");
+  var validLength = false;
+  var trackerResult = " ";
+
   var triangleSides = [lengtha.value, lengthb.value, lengthc.value];
 
-  //CONTROL OF INPUTTED LENGTHS TO BE VALID NUMBERS
-  var validLength = false;
-  for (var indexArray = 0; indexArray < triangleSides.length; indexArray++) {
-    if (isNaN(triangleSides[indexArray]) === false){
-         validLength = true;
-     } else if (triangleSides[indexArray] > 0){
-         validLength = true;
-     } else {
-         validLength = false;
-     }
-    }
-  }
-  //end of for loop (end of control valid numbers)
+  var lengtha = triangleSides[0];
+  var lengthb = triangleSides[1];
+  var lengthc = triangleSides[2];
 
-  if (validLength === false) {
-    trackerResult = "/!\\WARNING/!\\ \n PLEASE ENSURE THAT YOU ENTERED POSITIVE NUMBERS AND SUBMIT AGAIN!";
+//   //CONTROL OF INPUTTED LENGTHS TO BE VALID NUMBERS
+//   var validLength = false;
+//   for (var indexArray = 0; indexArray < triangleSides.length; indexArray++) {
+//     if (isNaN(triangleSides[indexArray]) === false){
+//          validLength *= true;
+//      } else if (triangleSides[indexArray] > 0){
+//          validLength *= true;
+//      } else {
+//          validLength = false;
+//      }
+//     }
+//   //end of for loop (end of control valid numbers)
+//
+//   var isTriangle = function (lengtha, lengthb, lengthc) {
+//     return (lengtha + lengthb > lengthc || lengthb + lengthc >lengtha || lengtha +lengthc > lengthb)
+//   }
+//
+//
+//   var triangleTracker = function(lengtha, lengthb, lengthc) {
+//     if (!(isTriangle(lengtha, lengthb, lengthc))){
+//       trackerResult = "This is not a triangle";
+//     } else if (lengtha === lengthb && lengthb === lengthc){
+//       trackerResult = "This is an equilateral triangle";
+//     } else if (lengtha === lengthb || lengthb === lengthc || lengtha === lengthc) {
+//       trackerResult = "This is an isoscele triangle";
+//     } else {
+//       trackerResult = "This is a scalene triangle";
+//     }
+//   }
+//
+//   //real tracker control flow
+//   if (validLength === false) {
+//     trackerResult = "/!\\WARNING/!\\ \n PLEASE ENSURE THAT YOU ENTERED POSITIVE NUMBERS AND SUBMIT AGAIN!";
+//   } else {
+//       triangleTracker();
+//   }
+// //end of tracker control flow
+
+var triangle = function(lengtha, lengthb, lengthc){
+  if (isTriangle(lengtha, lengthb, lengthc)) {
+    return triangleType(lengtha, lengthb, lengthc)
   } else {
-      if (lengtha.value === lengthb.value && lengthb.value === lengthvc.value){
-        trackerResult = "This is an equilateral triangle";
-      } else if (lengtha.value + lengthb.value <= lengthc.value || lengthb.value + lengthc.value <= lengthb.value || lengtha.value +lengthc.value <= lengthb.value) {
-          trackerResult = "This is not a triangle";
-      } else {
-        if (lengtha === lengthb || lengthb === lengthc || lengtha === lengthc) {
-          trackerResult = "This is an isoscele triangle";
-        } else {
-          trackerResult = "This is a scalene triangle";
-        }
-      }
+    trackerResult = "This is not a triangle";
   }
+};
 
-$("text_where").innerHTML = trackerResult;
-// }
-// closing bracket of display() function
+var triangleType = function(lengtha, lengthb, lengthc){
+  if (lengtha===lengthb && lengthb===lengthc) {
+    trackerResult = "This is an equilateral triangle";
+  } else if (lengtha===lengthb || lengtha===lengthc || lengthc===lengthb) {
+    trackerResult = "This is an isoscele triangle";
+  } else {
+    trackerResult = "This is a scalene triangle";
+  }
+};
 
-// for (var notNumber = true; notNumber === true;) {
-//     var countBy = prompt("Enter a number to count by:");
-//  var testCountBy = parseInt(countBy);
-//  if (isNaN(testCountBy) === true){
-//      notNumber = true;
-//  } else if (testCountBy <= 0){
-//      notNumber = true;
-//  } else {
-//      notNumber = false;
-//  }
-// }
+var isTriangle = function(lengtha, lengthb, lengthc) {
+  return lengtha <= (lengthb + lengthc) && lengthb <= (lengthc + lengtha) && lengthc <= (lengtha + lengthb)
+};
+
+
+
+triangle(lengtha, lengthb, lengthc);
+
+
+
+
+
+console.log(trackerResult);
+  $("text_where").innerHTML = trackerResult;
+};
