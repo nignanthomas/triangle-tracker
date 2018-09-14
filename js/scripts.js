fd395293e68c8770efcace6ef7a087f1a5b571c3
nignanthomas <nignanthomas@gmail.com>
2018-09-14 08:19:47 +0300

    perform a control flow on the array for valid numbers and innerHTML to return the result of the tracker

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,35 @@ var $ = function(id) { return document.getElementById(id); }
 var lengtha = $("lengtha");
 var lengthb = $("lengthb");
 var lengthc = $("lengthc");
+var trackerResult;
 
 // var triangleSides = [$("lengtha"), $("lengthb"), $("lengthc")];
-
+//THIS FUNCTION WILL RETURN THE RESULT OF THE TRACKER IN HTML
 var display = function (){
   var lengtha = $("lengtha");
   var lengthb = $("lengthb");
   var lengthc = $("lengthc");
   var triangleSides = [lengtha.value, lengthb.value, lengthc.value];
 
+  //CONTROL OF INPUTTED LENGTHS TO BE VALID NUMBERS
+  var validLength = false;
+  for (var indexArray = 0; indexArray < triangleSides.length; indexArray++) {
+    if (isNaN(triangleSides[indexArray]) === false){
+         validLength = true;
+     } else if (triangleSides[indexArray] > 0){
+         validLength = true;
+     } else {
+         validLength = false;
+     }
+    }
+  }
+  //end of for loop (end of control valid numbers)
 
+  
 
-};
+$("text_where").innerHTML = trackerResult;
+// }
+// closing bracket of display() function
 
 // for (var notNumber = true; notNumber === true;) {
 //     var countBy = prompt("Enter a number to count by:");
