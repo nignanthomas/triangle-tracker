c18c65b2e942323f834a62574ff3beeba09fecfc
nignanthomas <nignanthomas@gmail.com>
2018-09-14 07:23:29 +0300

    correction on the script to add .value property to collect the inputted data and add function display(); to onclick button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
 var $ = function(id) { return document.getElementById(id); }
 
-var triangleSides = [$(lengtha), $(lengthb), $(lengthc)];
+
+var lengtha = $("lengtha");
+var lengthb = $("lengthb");
+var lengthc = $("lengthc");
+
+// var triangleSides = [$("lengtha"), $("lengthb"), $("lengthc")];
+
+var display = function (){
+  var lengtha = $("lengtha");
+  var lengthb = $("lengthb");
+  var lengthc = $("lengthc");
+  var triangleSides = [lengtha.value, lengthb.value, lengthc.value];
+
+
+
+};
+
+// for (var notNumber = true; notNumber === true;) {
+//     var countBy = prompt("Enter a number to count by:");
+//  var testCountBy = parseInt(countBy);
+//  if (isNaN(testCountBy) === true){
+//      notNumber = true;
+//  } else if (testCountBy <= 0){
+//      notNumber = true;
+//  } else {
+//      notNumber = false;
+//  }
+// }
